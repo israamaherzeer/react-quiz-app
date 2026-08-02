c4b165e4916e2661a50d00e5b05bba54a7f21a0b
ISRAA <211106@ppu.edu.ps>
2026-08-02 21:51:59 +0300

    update

--- src/component/Loading/Loading.tsx
+++ src/component/Loading/Loading.tsx
@@ -7,10 +7,7 @@ const Loading = () => {
   return (
     <div className="loadingContainer">
 
-      <div className="loadingImage">
-        
-      </div>
-
+  
       <h2>Preparing your quiz...</h2>
 
       <p>
